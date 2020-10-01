d86d4c8c0673adaa4a8933a5e1d61d74267e4d48
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2020-10-01 17:33:17 -0300

    Entidade Produto, Mapeamento JPA N-N e Script SQL Completo

---
+++ backend/src/main/java/com/devsuperior/dscatalog/model/Produto.java
@@ -0,0 +1,134 @@
+package com.devsuperior.dscatalog.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "produtos")
+public class Produto implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nome;
+	
+	@Column(columnDefinition = "TEXT")
+	private String descricao;
+	
+	private BigDecimal preco;
+	
+	@Column(name = "img_url")
+	private String imgUrl;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant data;
+	
+	@ManyToMany
+	@JoinTable(name = "produto_categoria",  joinColumns = @JoinColumn(
+			   name = "produto_id"), inverseJoinColumns = @JoinColumn(
+			   name = "categoria_id"))
+	private Set<Categoria> categorias = new HashSet<>();
+
+	public Produto() {}
+
+	public Produto(Long id, String nome, String descricao, BigDecimal preco, String imgUrl, Instant data) {
+		this.id = id;
+		this.nome = nome;
+		this.descricao = descricao;
+		this.preco = preco;
+		this.imgUrl = imgUrl;
+		this.data = data;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public Instant getData() {
+		return data;
+	}
+
+	public void setData(Instant data) {
+		this.data = data;
+	}
+
+	public Set<Categoria> getCategorias() {
+		return categorias;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+}
--- backend/src/main/resources/import.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO categorias (nome, criado_em) VALUES ('Livros', NOW());
-INSERT INTO categorias (nome, criado_em) VALUES ('Eletrônicos', NOW());
-INSERT INTO categorias (nome, criado_em) VALUES ('Computadores', NOW());
-
