f1d05693393c7649f6f623faafbaf8c893b5a94d
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2020-09-30 22:34:33 -0300

    Transações, Sessão JPA e Seeding da Base de Dados

--- backend/src/main/java/com/devsuperior/dscatalog/service/CategoriaService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/service/CategoriaService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dscatalog.model.Categoria;
 import com.devsuperior.dscatalog.repository.CategoriaRepository;
@@ -14,6 +15,7 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 	
+	@Transactional(readOnly = true)
 	public List<Categoria> buscarTodas() {
 		return categoriaRepository.findAll();
 	}
---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO categorias (nome) VALUES ('Livros');
+INSERT INTO categorias (nome) VALUES ('Eletrônicos');
+INSERT INTO categorias (nome) VALUES ('Computadores');
