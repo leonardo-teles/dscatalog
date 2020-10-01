c7dc4240f2db8c7c97ac6ee0dc947e8ddc464260
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2020-10-01 12:10:39 -0300

    Buscando Categoria por Id

--- backend/src/main/java/com/devsuperior/dscatalog/resource/CategoriaResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resource/CategoriaResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,4 +25,12 @@ public class CategoriaResource {
 		
 		return ResponseEntity.ok().body(lista);
 	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<CategoriaDTO> buscarPorId(@PathVariable Long id) {
+		CategoriaDTO dto = categoriaService.buscarPorId(id);
+		
+		return ResponseEntity.ok().body(dto);
+	}
+	
 }
--- backend/src/main/java/com/devsuperior/dscatalog/service/CategoriaService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/service/CategoriaService.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dscatalog.service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +22,14 @@ public class CategoriaService {
 	public List<CategoriaDTO> buscarTodas() {
 		List<Categoria> lista =  categoriaRepository.findAll();
 
-		return  lista.stream().map(c -> new CategoriaDTO(c)).collect(Collectors.toList());
+		return lista.stream().map(c -> new CategoriaDTO(c)).collect(Collectors.toList());
+	}
+
+	@Transactional(readOnly = true)
+	public CategoriaDTO buscarPorId(Long id) {
+		Optional<Categoria> opt = categoriaRepository.findById(id);
+		Categoria categoria = opt.get();
+		
+		return new CategoriaDTO(categoria);
 	}
 }
