84c297b772418859b4a4cdf62b594b04ed85b15e
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2020-10-07 22:33:13 -0300

    Customizando o Bootstrap

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,29 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import './core/assets/styles/custom.scss'
+import './app.scss';
 
 const App = () => {
-const [counter, setCounter] = useState(0);
-
-    useEffect(() => {
-        console.log('mudou de valor')
-    }, [counter]);
 
     return (
-      <div className="container mt-5">
-          <button className="btn btn-primary mr-3" onClick={() => setCounter(counter + 1)} >
-              +
-          </button>
-          <span>
-              {counter}
-          </span>
-          <button className="btn btn-primary ml-3" onClick={() => setCounter(counter - 1)} >
-              -
-          </button>
-
-          <div>
-              {counter > 5 && <h1>O valor é maior do que 5</h1>}
-              {counter <= 5 && <h1>O valor é menor ou igual a 5</h1>}
-          </div>
-      </div>
+        <h1>App</h1>
     );
 }
 
