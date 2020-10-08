799d9ad54c36492582688e31ad74bf278f9e640c
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2020-10-07 22:05:08 -0300

    Componente com Estado e Ciclos de Vida

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
-import Alert from './Alert';
+import React, { useState, useEffect } from 'react';
 
 const App = () => {
+const [counter, setCounter] = useState(0);
+
+    useEffect(() => {
+        console.log('mudou de valor')
+    }, [counter]);
+
     return (
       <div className="container mt-5">
-          <Alert text="Leonardo"/>
-          <Alert text="Manuela"/>
-          <Alert />
+          <button className="btn btn-primary mr-3" onClick={() => setCounter(counter + 1)} >
+              +
+          </button>
+          <span>
+              {counter}
+          </span>
+          <button className="btn btn-primary ml-3" onClick={() => setCounter(counter - 1)} >
+              -
+          </button>
+
+          <div>
+              {counter > 5 && <h1>O valor é maior do que 5</h1>}
+              {counter <= 5 && <h1>O valor é menor ou igual a 5</h1>}
+          </div>
       </div>
     );
 }
