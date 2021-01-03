709095c850b8df8ece587b921b770233ef1909d2
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-01-03 15:00:04 -0300

    Estrutura Genérica para Lidar com Vários Campos de Formulário

--- front-web/src/pages/admin/components/produtos/formulario/index.tsx
+++ front-web/src/pages/admin/components/produtos/formulario/index.tsx
@@ -1,17 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FormularioBase from '../../formularioBase';
 
 import './styles.scss';
 
+type FormState = {
+    nome: string;
+    preco: string;
+    categoria: string;
+}
+
 const Formulario = () => {
+
+    const [formData, setFormData] = useState<FormState>({
+        nome: '',
+        preco: '',
+        categoria: ''
+    });
+    
+    const handleOnChange = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+        const name = event.target.name;
+        const value = event.target.value;
+
+        setFormData(data => ({ ...data, [name]: value }));
+    }
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        console.log(formData);
+    }
+
     return (
-        <FormularioBase titulo="CADASTRAR PRODUTO">
-            <div className="row">
-                <div className="col-6">
-                    <input type="text" className="form-control"/>
+        <form onSubmit={handleSubmit}>
+            <FormularioBase titulo="cadastrar produto">
+
+                <div className="row">
+                    <div className="col-6">
+                        <input 
+                            value={formData.nome}
+                            name="nome"
+                            type="text" 
+                            className="form-control mb-3"
+                            onChange={handleOnChange}
+                            placeholder="Nome do prduto"
+                        />
+
+                        <select 
+                            value={formData.categoria}
+                            name="categoria"
+                            className="form-control mb-3" 
+                            onChange={handleOnChange}
+                        >
+                            <option value="computador">Computador</option>
+                            <option value="livro">Livro</option>
+                            <option value="eletronico">Eletrônicos</option>
+                        </select>                    
+
+                        <input 
+                            value={formData.preco}
+                            name="preco"
+                            type="text" 
+                            className="form-control"
+                            onChange={handleOnChange}
+                            placeholder="Preço"
+                        />
+                    </div>
                 </div>
-            </div>
-        </FormularioBase>
+            </FormularioBase>
+        </form>
     )
 }
 
