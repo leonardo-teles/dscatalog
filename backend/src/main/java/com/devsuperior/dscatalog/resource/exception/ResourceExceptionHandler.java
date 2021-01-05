9dc89b4fb3894aa8349e4412e143cd68506e48cb
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-01-04 23:35:37 -0300

    Tratando Exceção MethodArgumentNotValidException

--- backend/src/main/java/com/devsuperior/dscatalog/resource/exception/ResourceExceptionHandler.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resource/exception/ResourceExceptionHandler.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -30,5 +32,17 @@ public class ResourceExceptionHandler {
 		
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+		
+		ValidationError error = new ValidationError(Instant.now(), HttpStatus.UNPROCESSABLE_ENTITY.value(), "Erro de Validação", e.getMessage(), request.getRequestURI());
+		
+		for(FieldError fe : e.getBindingResult().getFieldErrors()) {
+			error.addError(fe.getField(), fe.getDefaultMessage());
+		}
+		
+		return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(error);
+	}	
 	
 }
