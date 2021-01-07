c6e935631218b21048e3ce60da82d5b0c47a7b81
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-01-06 21:37:21 -0300

    Externalizando a Configuração

--- backend/src/main/java/com/devsuperior/dscatalog/config/AuthorizationServerConfig.java
+++ backend/src/main/java/com/devsuperior/dscatalog/config/AuthorizationServerConfig.java
@@ -3,6 +3,7 @@ package com.devsuperior.dscatalog.config;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -21,6 +22,15 @@ import com.devsuperior.dscatalog.components.JwtTokenEnhancer;
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
+	@Value("${security.oauth2.client.client-id}")
+	private String clientId;
+	
+	@Value("${security.oauth2.client.client-secret}")
+	private String clientSecret;
+	
+	@Value("${jwt.duration}")
+	private Integer jwtDuration;
+	
 	@Autowired
 	private BCryptPasswordEncoder encoder;
 	
@@ -44,11 +54,11 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		clients.inMemory()
-			   .withClient("dscatalog")
-			   .secret(encoder.encode("dscatalog123"))
+			   .withClient(clientId)
+			   .secret(encoder.encode(clientSecret))
 			   .scopes("read", "write")
 			   .authorizedGrantTypes("password")
-			   .accessTokenValiditySeconds(86400);
+			   .accessTokenValiditySeconds(jwtDuration);
 	}
 
 	@Override
