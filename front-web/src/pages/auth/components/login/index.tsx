5fdc1efe1731c8c605bd28cc06b9fb9a611fee2f
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-01-19 23:03:51 -0300

    Biblioteca React Hook Form para Gerenciar Formulários

--- front-web/src/pages/auth/components/login/index.tsx
+++ front-web/src/pages/auth/components/login/index.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 import AuthCard from '../card';
 import ButtonIcon from 'core/components/buttonIcon';
 
 import './styles.scss';
 
+type FormData = {
+    email: string;
+    senha: string;
+}
+
 const Login = () => {
+    const { register, handleSubmit } = useForm<FormData>();
+
+    const onSubmit = (data: FormData) => {
+        console.log(data);
+    }
+
     return (
         <AuthCard titulo="login">
-            <form className="login-form">
-                <input type="email" className="form-control input-base margin-bottom-30" placeholder="e-Mail" />
-                <input type="password" className="form-control input-base" placeholder="senha" />
+            <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
+                <input type="email" className="form-control input-base margin-bottom-30" placeholder="e-Mail" name="email" ref={register}/>
+                <input type="password" className="form-control input-base" placeholder="senha" name="senha" ref={register}/>
                 <Link to="/admin/auth/recuperar" className="login-link-recuperar">
                     Esqueceu a senha?
                 </Link>
