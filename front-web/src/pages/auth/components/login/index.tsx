58bff612872b91b66e60e234ad39677999e5faab
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-01-19 22:29:39 -0300

    Criando Formulário de Login

--- front-web/src/pages/auth/components/login/index.tsx
+++ front-web/src/pages/auth/components/login/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import AuthCard from '../card';
+import ButtonIcon from 'core/components/buttonIcon';
 
 import './styles.scss';
 
@@ -7,7 +9,22 @@ const Login = () => {
     return (
         <AuthCard titulo="login">
             <form className="login-form">
-                <h3>Formulário de login</h3>
+                <input type="email" className="form-control input-base margin-bottom-30" placeholder="e-Mail" />
+                <input type="password" className="form-control input-base" placeholder="senha" />
+                <Link to="/admin/auth/recuperar" className="login-link-recuperar">
+                    Esqueceu a senha?
+                </Link>
+                <div className="login-submit">
+                    <ButtonIcon text="logar" />
+                </div>
+                <div className="text-center">
+                    <span className="nao-registrado">
+                        Não tem cadastro?
+                    </span>
+                    <Link to="/admin/auth/cadastro" className="login-link-cadastrar">
+                        cadastrar
+                    </Link>
+                </div>
             </form>
         </AuthCard>
     );
