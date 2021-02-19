39ada9c92e1061c5ff9092ac817697d905cf7e5b
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-02-18 22:17:34 -0300

    Exibindo Progresso de Upload

--- front-web/src/pages/admin/components/produtos/uploadImagem/index.tsx
+++ front-web/src/pages/admin/components/produtos/uploadImagem/index.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ReactComponent as UploadPlaceholder } from 'core/assets/imagens/upload-placeholder.svg';
 
 import './styles.scss';
 import { makePrivateRequest } from 'core/utils/request';
+import { toast } from 'react-toastify';
 
 const UploadImagem = () => {
+
+    const [progressoUpload, setProgressoUpload] = useState(0);
+    
+    const onUploadProgress = (progressEvent: ProgressEvent) => {
+        const progressoCarregamento = Math.round((progressEvent.loaded * 100) / progressEvent.total);
+
+        setProgressoUpload(progressoCarregamento);
+    }
+    
     const upload = (imagemSelecionada: File) => {
         const payload = new FormData();
         payload.append('arquivo', imagemSelecionada);
@@ -12,14 +22,16 @@ const UploadImagem = () => {
         makePrivateRequest({
             url: '/produtos/imagem', 
             method: 'post',
-            data: payload
+            data: payload,
+            onUploadProgress
         })
         .then(() => {
             console.log('arquivo enviado com sucesso');
         })
         .catch(() => {
-            console.log('erro ao enviar arquivo');
+            toast.error('Erro ao enviar arquivo');        
         })
+        .finally(() => setProgressoUpload(0))
     }
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -50,7 +62,7 @@ const UploadImagem = () => {
             <div className="col-6 upload-placeholder">
                 <UploadPlaceholder />
                 <div className="container-progresso-upload">
-                    <div className="progresso-upload">
+                    <div className="progresso-upload" style={{ width: `${progressoUpload}%` }}>
 
                     </div>
                 </div>
