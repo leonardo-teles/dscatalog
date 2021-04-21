aa324d818988b9e6b030e83fb711f29111849531
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-04-21 15:33:00 -0300

    Atualizando Categoria com PUT

--- backend/src/main/java/com/devsuperior/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/resources/CategoryResource.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,7 +38,7 @@ public class CategoryResource {
 		return ResponseEntity.ok().body(dto);
 	}
 	
-	@PostMapping
+  	@PostMapping
 	public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto) {
 		dto = service.insert(dto);
 		URI uri = ServletUriComponentsBuilder
@@ -47,5 +48,11 @@ public class CategoryResource {
 		
 		return ResponseEntity.created(uri).body(dto);
 	}
-	
+
+  	@PutMapping(value = "/{id}")
+	public ResponseEntity<CategoryDTO> update(@PathVariable Long id, @RequestBody CategoryDTO dto) {
+		dto = service.update(id, dto);
+		
+		return ResponseEntity.ok().body(dto);	
+	}
 }
