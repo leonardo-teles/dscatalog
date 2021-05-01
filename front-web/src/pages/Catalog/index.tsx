563d347bf1935e4e5bec397f1d9ca7c8e180b3f2
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-04-30 22:40:29 -0300

    Criando Utilitário para Fazer Requisições HTTP

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,15 +1,22 @@
 import React, { useEffect } from 'react';
-
 import { Link } from 'react-router-dom';
+
 import ProductCard from './components/ProductCard';
+import { makeRequest } from '../../core/utils/request';
 
 import './styles.scss';
 
 const Catalog = () => {
 
+    //quando o componente iniciar, buscar a lista de produtos
     useEffect(() => {
-        fetch('http://localhost:3000/products')
-            .then(response => response.json())
+        const params = {
+            page: 0,
+            linesPerPage: 12
+        }
+
+
+        makeRequest({ url: '/products', params })
             .then(response => console.log(response));
     }, []);
 
