6de11d2d97f756d25e40e1add146528d60ce0c80
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-05-13 15:31:58 -0300

    Renderizando os Itens da Paginação Dinamicamente

--- front-web/src/core/components/Pagination/index.tsx
+++ front-web/src/core/components/Pagination/index.tsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import { ReactComponent as ArrowIcon } from 'core/assets/images/arrow.svg';
+import { generateList } from 'core/utils/list';
 
 import './styles.scss';
 
-const Pagination = () => {
+type Props = {
+    totalPages: number;
+    activePage: number;
+    onChange: (item: number) => void;
+}
+
+const Pagination = ({ totalPages, activePage, onChange }: Props) => {
+    const items = generateList(totalPages);
+
     return (
         <div className="pagination-container">
             <ArrowIcon className="pagination-previous"/>
-            <div className="pagination-item active">
-                1
-            </div>
+
+            {items.map(item => (
+                <div 
+                    key={item}
+                    className={`pagination-item ${item === activePage ? 'active' : ''}`}
+                    onClick={() => onChange(item)}
+                >
+                    {item + 1}
+                </div>
+            ))}
+
             <ArrowIcon className="pagination-next"/>
         </div>
     );
--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -12,10 +12,11 @@ import './styles.scss';
 const Catalog = () => {
     const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
     const [isLoading, setIsLoading] = useState(false);
+    const [activePage, setActivePage] = useState(0);
 
     useEffect(() => {
         const params = {
-            page: 0,
+            page: activePage,
             linesPerPage: 12
         }
 
@@ -25,7 +26,7 @@ const Catalog = () => {
             .finally(() => {
                 setIsLoading(false);
             });
-    }, []);
+    }, [activePage]);
 
     return (
         <div className="catalog-container">
@@ -41,7 +42,13 @@ const Catalog = () => {
                     ))
                 )}
             </div>
-            <Pagination />
+            {productsResponse && (
+                <Pagination 
+                    totalPages={productsResponse.totalPages}
+                    activePage={activePage}
+                    onChange={page => setActivePage(page)}
+                />
+            )}
         </div>
     );
 }
