40cf2a39576c574f8b2cf95713fa07543f2ee186
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-05-13 22:20:54 -0300

    Criando a Navegação Inicial das Rotas do Admin

--- front-web/src/pages/Admin/index.tsx
+++ front-web/src/pages/Admin/index.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Route, Switch } from 'react-router';
 import Navbar from './components/Navbar';
 
+import './styles.scss';
+
 const Admin = () => (
-    <div>
+    <div className="admin-container">
         <Navbar />
+        <div className="admin-content">
+            <Switch>
+                <Route path="/admin/products">
+                    <h4>Produtos</h4>
+                </Route>
+
+                <Route path="/admin/categories">
+                    <h4>Categorias</h4>
+                </Route>
+                <Route path="/admin/users">
+                    <h4>Usuários</h4>
+                </Route>
+            </Switch>
+        </div>
     </div>
 );
 
