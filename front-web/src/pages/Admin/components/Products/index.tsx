af9c4839f0a5dff7493c44cdd1e0be28b1d07bb0
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-06-09 11:33:27 -0300

    Criando a Estrutura Base de Formulários no Admin

--- front-web/src/pages/Admin/components/Products/index.tsx
+++ front-web/src/pages/Admin/components/Products/index.tsx
@@ -1,29 +1,18 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
-import { Link } from 'react-router-dom';
+import Form from './Form';
+import List from './List';
 
 const Products = () => {
     return (
         <div>
-            <Link to="/admin/products" className="mr-5">
-                Listar produtos
-            </Link>
-
-            <Link to="/admin/products/create" className="mr-5">
-                Criar produto
-            </Link>
-
-            <Link to="/admin/products/10" className="mr-5">
-                Editar produto
-            </Link>
-
             <Switch>
                 <Route path="/admin/products" exact>
-                    <h3>Exibir a listagem de produtos</h3>
+                    <List />
                 </Route>
 
                 <Route path="/admin/products/create">
-                    <h3>Criar novo produtos</h3>
+                    <Form />
                 </Route>
 
                 <Route path="/admin/products/:productId">
