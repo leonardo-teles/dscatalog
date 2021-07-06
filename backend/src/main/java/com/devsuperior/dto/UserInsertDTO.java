c6dfbc1df3cecdffc160d3637d19d8746c6b029b
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-07-06 12:39:55 -0300

    Implementando um ConstraintValidation Customizado

--- backend/src/main/java/com/devsuperior/dto/UserInsertDTO.java
+++ backend/src/main/java/com/devsuperior/dto/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package com.devsuperior.dto;
 
+import com.devsuperior.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO {
 	private static final long serialVersionUID = 1L;
 
---
+++ backend/src/main/java/com/devsuperior/services/validation/UserInsertValidator.java
@@ -0,0 +1,43 @@
+package com.devsuperior.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.devsuperior.dto.UserInsertDTO;
+import com.devsuperior.entities.User;
+import com.devsuperior.repositories.UserRepository;
+import com.devsuperior.resources.exception.FieldMessage;
+
+public class UserInsertValidator implements ConstraintValidator<UserInsertValid, UserInsertDTO> {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	@Override
+	public void initialize(UserInsertValid ann) {}
+
+	@Override
+	public boolean isValid(UserInsertDTO dto, ConstraintValidatorContext context) {
+		
+		List<FieldMessage> list = new ArrayList<>();
+		
+		User user = repository.findByEmail(dto.getEmail());
+		
+		if(user != null) {
+			list.add(new FieldMessage("email", "e-Mail já existe"));
+		}
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName()).addConstraintViolation();
+		}
+		
+		return list.isEmpty();
+	}
+}
+
