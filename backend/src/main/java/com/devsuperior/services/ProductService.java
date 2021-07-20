38b86c4f41c69628be1b9d8b9ccca8ef69906000
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-07-20 17:30:25 -0300

    Lidando com o Problema N+1 Consultas

--- backend/src/main/java/com/devsuperior/services/ProductService.java
+++ backend/src/main/java/com/devsuperior/services/ProductService.java
@@ -36,9 +36,11 @@ public class ProductService {
 	public Page<ProductDTO> findAllPaged(Long categoryId, String name, PageRequest pageRequest) {
 		List<Category> categories = (categoryId == 0) ? null : Arrays.asList(categoryRepository.getOne(categoryId));
 		
-		Page<Product> list = repository.find(categories, name, pageRequest);
+		Page<Product> page = repository.find(categories, name, pageRequest);
 		
-		return list.map(x -> new ProductDTO(x));
+		repository.findProductsWithCategories(page.getContent());
+		
+		return page.map(x -> new ProductDTO(x, x.getCategories()));
 	}
 
 	@Transactional(readOnly = true)
